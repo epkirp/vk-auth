5cc7a6bf850acdc8591859cd5cd61fbffb9d69c8
elena_k <elena_k@webant.ru>
2020-11-24 19:34:58 +0300

    first commit

---
+++ app/src/main/java/ru/webant/auth/MainActivity.kt
@@ -0,0 +1,39 @@
+package ru.webant.auth
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.vk.api.sdk.VK
+import com.vk.api.sdk.auth.VKAccessToken
+import com.vk.api.sdk.auth.VKAuthCallback
+import com.vk.api.sdk.auth.VKScope
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnSignIn.setOnClickListener {
+            VK.login(this, arrayListOf(VKScope., VKScope.PHOTOS, VKScope.EMAIL))
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val callback = object : VKAuthCallback {
+            override fun onLogin(token: VKAccessToken) {
+                // User passed authorization
+                val a = 0
+
+            }
+
+            override fun onLoginFailed(errorCode: Int) {
+                // User didn't pass authorization
+                val a = 0
+            }
+        }
+        if (data == null || !VK.onActivityResult(requestCode, resultCode, data, callback)) {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+    }
+}
